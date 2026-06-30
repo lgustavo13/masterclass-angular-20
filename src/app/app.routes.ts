e2ef6e8e3283396c4d075ca2196af88c9a7906ae
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-06-30 17:01:55 -0300

    feat: create initial routes of screens pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'counter',
+    loadComponent: () => import('./screens/counter/counter').then((m) => m.Counter),
+  },
+  {
+    path: 'todos',
+    loadComponent: () => import('./screens/todos/todos').then((m) => m.Todos),
+  },
+];
