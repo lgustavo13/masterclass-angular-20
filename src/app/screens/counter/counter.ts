edb52587cb04e95fecce9780fba49d1bc6977154
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-06-30 17:33:16 -0300

    feat: create title component

--- src/app/screens/counter/counter.ts
+++ src/app/screens/counter/counter.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from '../../components/shared/title/title';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [Title],
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
-export class Counter {
-
-}
+export class Counter {}
--- src/app/screens/todos/todos.ts
+++ src/app/screens/todos/todos.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from '../../components/shared/title/title';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [Title],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
-export class Todos {
-
-}
+export class Todos {}
